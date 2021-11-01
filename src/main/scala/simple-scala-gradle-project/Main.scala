f446dda90b601a7b43666245e59b242083917ce0
AnandhuCC <91460743+AnandhuCC@users.noreply.github.com>
2021-11-01 15:28:45 +0530

    add non compliant code (not a syntax error)

--- src/main/scala/simple-scala-gradle-project/Main.scala
+++ src/main/scala/simple-scala-gradle-project/Main.scala
@@ -1,10 +1,8 @@
 package example
 
 object Main extends App {
-  println(List(1, 2, 3).toString)
-  if (x == 0) {
-  doSomething
-} else if (x == 1) {
-  doSomethingElse
-}
+  
+  if (!(a == 2)) {
+    println(List(1, 2, 3).toString)
+  }
 }
